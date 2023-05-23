3757f8b21a3bb68faeab4f6df8105f505dcba6ee
baeian <ddoory1103@gmail.com>
2023-05-23 15:25:20 +0900

    FEAT:: 할 일 삭제기능 추가

--- src/main/resources/static/js/todoAjax.js
+++ src/main/resources/static/js/todoAjax.js
@@ -16,3 +16,15 @@ function dataSend(){
         alert(JSON.stringify(error));
     });
 }
+
+function deleteEntity(id){
+    $.ajax({
+            type: 'DELETE',
+            url: '/todo/api/' + id,
+            contentType: 'application/json; charset=utf-8',
+        }).done(function(){
+            window.location.href='/';
+        }).fail(function(error){
+            alert(JSON.stringify(error));
+        });
+}
